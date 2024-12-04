8306e25e3d553793a3fca470966f2edf9d8f2f64
happyhere-TN <odhahri855@gmail.com>
2024-12-04 13:09:51 -0800

    Add files via upload

---
+++ xashsetup.sh
@@ -0,0 +1,8 @@
+clear && echo "
+[[ Termux - xashDS]] this update for xash3D-new-engine (cant join with old-engine)
+ 
+    
+	wait to extaracting xashds on termux....
+      " && cd && cd /storage/emulated/0/download/termux-xash3D/tr && cp -rf bash.bashrc /data/data/com.termux/files/usr/etc && cd .. && cd db && cp -rf bash.bashrc /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/etc && cd .. && cd gamelib && cp -rf xash  /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/debian/usr/games && echo "
+
+     the installation were successful!, now close and open termux again to start the server :)"
