04b96e65f22b73523d98e87d416030b5f120eff0
happyhere-TN <odhahri855@gmail.com>
2024-12-06 07:30:40 -0800

    Add files via upload

--- storage/emulated/0/data/data/com.termux/files/home/xashsetup.sh
+++ storage/emulated/0/data/data/com.termux/files/home/xashsetup.sh
@@ -1,8 +1,33 @@
-clear && echo "
-[[ Termux - xashDS]] this update for xash3D-new-engine and old engine
- 
-    
-	wait to extaracting xashds on termux....
-      " && cd && wget https://raw.githubusercontent.com/AllPlatform/Termux-UbuntuX86_64/master/Ubuntu-i386.sh && chmod +x * && ./Ubuntu-i386.sh && cd && cd /storage/emulated/0/download/termux-xash3D/tr && cp -rf bash.bashrc /data/data/com.termux/files/usr/etc && cd .. && cd db && cp -rf bash.bashrc /data/data/com.termux/files/home/ubuntu-fs/etc && cd .. && cd gamelib && cp -rf xash  /data/data/com.termux/files/home/ubuntu-fs/usr/games && clear && echo "
+#!/bin/bash
 
-     the installation were successful!, now close and open termux again to start the server :)"
+#fast shell's
+alias copytr="cd && cd /storage/emulated/0/download/termux-xash3D"
+alias installubuntu="clear && cd && wget https://raw.githubusercontent.com/AllPlatform/Termux-UbuntuX86_64/master/Ubuntu-i386.sh && chmod +x * && ./Ubuntu-i386.sh && clear"
+alias db=" cd && copytr && cd db && cp -rf bash.bashrc /data/data/com.termux/files/home/ubuntu-fs/etc"
+alias tr="cd && copytr && cd tr && cp -rf bash.bashrc /data/data/com.termux/files/usr/etc"
+alias mkxashdir="cd && cd ubuntu-fs/usr/games && mkdir xash"
+
+
+#install & extract
+echo "installing ubuntu ..."
+installubuntu
+echo "
+ [[ Termux - xashDS]] this update for xash3D-new-engine and old engine
+
+"
+db
+echo "remplacing ubuntu bash.bashrc file ...
+"
+sleep 3
+tr
+echo "remplacing termux bash.bashrc file  ...
+"
+sleep 3
+mkxashdir
+echo "building xash folder ...
+ "
+sleep 3
+echo "
+"
+#instalation done
+echo "the installation were successful!, now close and open termux again to start the server :)"
